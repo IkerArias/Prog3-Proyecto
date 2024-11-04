99d56d505f9e5871ca98944246f9d2f9baef1cfb
telmoolarreaga <telmo.olarreaga@opendeusto.es>
2024-11-04 19:21:56 +0100

    Creacion clase jugador para editar la vista de los jugadores en la clase ManagerMercado

---
+++ src/gui/ManagerJugador.java
@@ -0,0 +1,31 @@
+package gui;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import basicas.Jugador;
+
+public class ManagerJugador extends JFrame{
+	
+	public ManagerJugador(Jugador jugador) {
+        setTitle("Ficha del Jugador");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        
+        
+        panel.add(new JLabel("Nombre: " + jugador.getNombre()));
+        panel.add(new JLabel("Equipo: " + jugador.getEquipo()));
+        panel.add(new JLabel("Posición: " + jugador.getPosicion()));
+        panel.add(new JLabel("País: " + jugador.getPais()));
+        panel.add(new JLabel("Valor: " + jugador.getValor()));
+
+        add(panel);
+        this.setVisible(true);
+    }
+
+}
